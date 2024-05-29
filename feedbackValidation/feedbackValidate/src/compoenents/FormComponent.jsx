d533e525aba297e88fdef34f679117dce20c6873
Souptik18 <souptiksarkar2017@gmail.com>
2024-05-29 12:56:22 +0530

    update

--- feedbackValidation/feedbackValidate/src/compoenents/FormComponent.jsx
+++ feedbackValidation/feedbackValidate/src/compoenents/FormComponent.jsx
@@ -10,28 +10,26 @@ function FormComponent() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const checkName = /\d/;
-    const checkPhoneRegex = /[a-zA-Z]/;
-    const checkEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const pincodeRegex = /[a-zA-Z]/;
-    const checkPhone = checkPhoneRegex.test(data.phone);
-    const emailCheck = checkEmailRegex.test(data.email);
-    const namechk = checkName.test(data.name);
-    const checkPincode = pincodeRegex.test(data.pincode);
-    if (namechk) {
+
+    const nameContainsNumber = /\d/.test(data.name);
+    const phoneContainsLetter = /[a-zA-Z]/.test(data.phone);
+    const emailIsValid = /@.*\./.test(data.email);
+    const pincodeContainsLetter = /[a-zA-Z]/.test(data.pincode);
+
+    if (nameContainsNumber) {
       alert("Name cannot contain numbers");
-    } else if (checkPhone) {
+    } else if (phoneContainsLetter) {
       alert("Phone number cannot contain letters");
-    } else if (!emailCheck) {
+    } else if (!emailIsValid) {
       alert("Email should contain @ and .");
-    } else if (checkPincode) {
+    } else if (pincodeContainsLetter) {
       alert("Pincode cannot contain letters");
     } else {
       console.log("data sent");
     }
   };
 
-  const handleonChange = (e) => {
+  const handleChange = (e) => {
     setData({
       ...data,
       [e.target.name]: e.target.value,
@@ -40,13 +38,7 @@ function FormComponent() {
 
   return (
     <div>
-      <form
-        className="form-container"
-        action=""
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
+      <form className="form-container" onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
         <input
           type="text"
@@ -54,19 +46,21 @@ function FormComponent() {
           id="name"
           required
           placeholder="Enter Name"
-          onChange={(e) => handleonChange(e)}
+          onChange={handleChange}
           value={data.name}
         />
+
         <label htmlFor="email">Email ID</label>
         <input
           type="text"
           name="email"
-          required
           id="email"
+          required
           placeholder="Enter Email"
-          onChange={handleonChange}
+          onChange={handleChange}
           value={data.email}
         />
+
         <label htmlFor="phone">Phone</label>
         <input
           type="text"
@@ -74,9 +68,10 @@ function FormComponent() {
           id="phone"
           required
           placeholder="Enter Phone"
-          onChange={handleonChange}
+          onChange={handleChange}
           value={data.phone}
         />
+
         <label htmlFor="pincode">Pincode</label>
         <input
           type="text"
@@ -84,9 +79,10 @@ function FormComponent() {
           id="pincode"
           required
           placeholder="Enter Pincode"
-          onChange={handleonChange}
+          onChange={handleChange}
           value={data.pincode}
         />
+
         <button type="submit">Submit</button>
       </form>
     </div>
